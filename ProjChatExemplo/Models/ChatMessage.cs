ad5e0e4beb206f932fdcc3a9ffe7460a6c48b787
unknown <aannddrree>
2021-04-12 08:35:54 -0300

    Implantação

---
+++ ProjChatExemplo/Models/ChatMessage.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjChatExemplo.Models
+{
+    public class ChatMessage
+    {
+        public Int64 destination { get; set; }
+        public User sender { get; set; }
+        public string message { get; set; }
+    }
+}
---
+++ ProjChatExemplo/Repositories/ConnectionsRepository.cs
@@ -0,0 +1,35 @@
+﻿using ProjChatExemplo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjChatExemplo.Repositories
+{
+    public class ConnectionsRepository
+    {
+        private readonly Dictionary<string, User> connections =
+            new Dictionary<string, User>();
+
+
+        public void Add(string uniqueID, User user)
+        {
+            if (!connections.ContainsKey(uniqueID))
+                connections.Add(uniqueID, user);
+        }
+
+        public string GetUserId(long id)
+        {
+            return (from con in connections
+                    where con.Value.key == id
+                    select con.Key).FirstOrDefault();
+        }
+
+        public List<User> GetAllUser()
+        {
+            return (from con in connections
+                    select con.Value
+            ).ToList();
+        }
+    }
+}
